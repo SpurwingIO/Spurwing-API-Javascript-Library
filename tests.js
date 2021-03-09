ee672c230c7518ded371cbd7f1230e2dac1e7d09
Ilya Nevolin <ilya@spurwing.io>
2021-03-09 15:30:12 +0100

    pid

--- tests.js
+++ tests.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 // https://www.chaijs.com/guide/styles/
 
 let sp = new Spurwing();
-const pid = '3311c352-3c5e-4009-b5c3-1d550cc84398';
+const PID = 'your-product-id';
 
 function dateNow() {
   let d = new Date();
@@ -19,22 +19,22 @@ function  dateTomorrow() {
   console.log('UNIT TEST 1:')
   let tz = "Europe/Brussels";
 
-  let A = await sp.get_appointment_types(pid, true)
+  let A = await sp.get_appointment_types(PID, true)
   console.log(A)
   A.should.have.lengthOf(3) // default 3
 
-  let B = await sp.get_days_available(pid, A[0].id, dateNow(), tz, false)
+  let B = await sp.get_days_available(PID, A[0].id, dateNow(), tz, false)
   console.log(B)
   expect(B.days_available.length).to.be.at.least(1) // at least one day available this month
   
-  let C = await sp.get_slots_available(pid, A[0].id, dateNow(), dateTomorrow(), false)
+  let C = await sp.get_slots_available(PID, A[0].id, dateNow(), dateTomorrow(), false)
   console.log(C)
   expect(C.slots_available.length).to.be.at.least(96) // each day has 96 15min slots (60*24 / 15 == 96)
 
   let slot = C.slots_available[5].date
   console.log(slot)
 
-  let D = await sp.complete_booking(pid, A[0].id, slot, tz, 'Ilya', 'Nevolin', 'ilya2@nevolin.be', '111-111-7777', 'My Contact Type');
+  let D = await sp.complete_booking(PID, A[0].id, slot, tz, 'Ilya', 'Nevolin', 'ilya2@nevolin.be', '111-111-7777', 'My Contact Type');
   console.log(D)
   D.should.have.property('appointment')
   expect(D.appointment.length).to.equal(60)
