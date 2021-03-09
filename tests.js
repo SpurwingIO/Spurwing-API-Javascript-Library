84b23117c4c8fb8d85d9b81adb9f7335a2b88910
Ilya Nevolin <ilya@spurwing.io>
2021-03-09 18:10:10 +0100

    unit tests improved

--- tests.js
+++ tests.js
@@ -30,26 +30,32 @@ async function runner(testname, func) {
     let tz = "Europe/Brussels";
 
     let A = await sp.get_appointment_types(PID, true)
-    console.log(A)
+    console.log({A})
     A.should.have.lengthOf(3) // default 3
 
     let B = await sp.get_days_available(PID, A[0].id, dateNow(), tz, false)
-    console.log(B)
+    console.log({B})
     expect(B.days_available.length).to.be.at.least(1) // at least one day available this month
 
     let C = await sp.get_slots_available(PID, A[0].id, dateNow(), dateTomorrow(), false)
-    console.log(C)
+    console.log({C})
     expect(C.slots_available.length).to.be.at.least(10) // each day has 96 15min slots (60*24 / 15 == 96)
 
     let slot = C.slots_available[5].date
     let D = await sp.complete_booking(PID, A[0].id, slot, tz, 'Ilya', 'Nevolin', 'ilya2@nevolin.be', '111-111-7777', 'My Contact Type');
-    console.log(D)
+    console.log({D})
     D.should.have.property('appointment')
     expect(D.appointment.length).to.equal(60)
 
+    let E = await sp.list_appointments(KEY, 1000, 0, PID)
+    console.log({E})
+    E.should.have.property('data')
+    E.data.should.have.property('appointments')
+    expect(E.data.appointmentsCount).to.be.at.least(1)
+
     let apid = D.appointment.id;
     let F = await sp.delete_appointment(apid, KEY)
-    console.log(F)
+    console.log({F})
     F.should.have.property('data')
     F.data.should.have.property('appointment')
     F.data.appointment.id.should.equal(D.appointment.id)
@@ -58,8 +64,10 @@ async function runner(testname, func) {
 
   await runner('TEST 2', (async() => {
     let sp = new Spurwing();
-    let E = await sp.list_appointments(KEY, 1000, 0, PID)
-    console.log(E)
+    let A = await sp.list_appointments(KEY, 1000, 0, PID)
+    console.log({A})
+    A.should.have.property('data')
+    A.data.should.have.property('appointments')
   }));
 
 })();
