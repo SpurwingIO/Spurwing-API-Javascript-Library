64af6f0dd2eaba817e8af44e4c7071b5ed4ad901
Ilya Nevolin <justdoitzlol@gmail.com>
2021-03-20 19:25:38 +0100

    bugfixes and cleanup

--- spurwing.js
+++ spurwing.js
@@ -11,22 +11,22 @@ const Spurwing = (function() {
     return this
   }
 
-  this.get_appointment_types = async function(provider_id, clients_can_book) {
-    return await this.HTTP('GET', this.API_URL + 'appointment_types.json', { provider_id, clients_can_book })
+  this.get_appointment_types = async function(provider_id, page_size, offset) {
+    return await this.HTTP('GET', this.API_URL + 'appointment_types.json', { provider_id, page_size, offset })
   }
-  this.get_days_available = async function(provider_id, appointment_type_id, date_from_month, timezone, org_level) {
-    return await this.HTTP('GET', this.API_URL + 'bookings/days_available.json', { provider_id, appointment_type_id, date_from_month, timezone, org_level })
+  this.get_days_available = async function(provider_id, appointment_type_id, date_from_month, organization_level, timezone) {
+    return await this.HTTP('GET', this.API_URL + 'bookings/days_available.json', { provider_id, appointment_type_id, date_from_month, organization_level, timezone })
   }
-  this.get_slots_available = async function(provider_id, appointment_type_id, start_date, end_date, org_level) {
-    return await this.HTTP('GET', this.API_URL + 'bookings/slots_available.json', { provider_id, appointment_type_id, start_date, end_date, org_level })
+  this.get_slots_available = async function(provider_id, appointment_type_id, start_date, end_date, organization_level, timezone) {
+    return await this.HTTP('GET', this.API_URL + 'bookings/slots_available.json', { provider_id, appointment_type_id, start_date, end_date, organization_level, timezone })
   }
-  this.complete_booking = async function(provider_id, appointment_type_id, date, timezone, first_name, last_name, email, phone_number, contact_type) {
-    return await this.HTTP('POST', this.API_URL + 'bookings/complete_booking.json', { provider_id, appointment_type_id, date, timezone, first_name, last_name, email, phone_number, contact_type })
+  this.complete_booking = async function(provider_id, appointment_type_id, email, first_name, last_name, date, contact_type, appointment_id, appointment_location_id, timezone, video_chat_url) {
+    return await this.HTTP('POST', this.API_URL + 'bookings/complete_booking.json', { provider_id, appointment_type_id, email, first_name, last_name, date, contact_type, appointment_id, appointment_location_id, timezone, video_chat_url })
   }
-  this.list_appointments = async function(authorization, page_size, offset, provider_id) {
-    return await this.HTTP('GET', this.API_URL + 'appointments', { page_size, offset, provider_id }, { authorization: 'Bearer ' + authorization })
+  this.list_appointments = async function(authorization, page_size, offset, appointment_category, load_attendees, load_providers, load_appointment_type) {
+    return await this.HTTP('GET', this.API_URL + 'appointments', { page_size, offset, appointment_category, load_attendees, load_providers, load_appointment_type }, { authorization: 'Bearer ' + authorization })
   }
-  this.delete_appointment = async function(appointment_id, authorization) {
+  this.delete_appointment = async function(authorization, appointment_id) {
     return await this.HTTP('DELETE', this.API_URL + 'appointments/' + appointment_id, {}, { authorization: 'Bearer ' + authorization })
   }
 
@@ -66,8 +66,11 @@ const Spurwing = (function() {
         if (!obj)
           return '';
         let urlData = '';
-        for (let x in obj)
+        for (let x in obj) {
+          if (typeof obj[x] === 'undefined' || obj[x] == null)
+            continue;
           urlData = urlData + x + '=' + encodeURIComponent(obj[x]) + '&';
+        }
         urlData = urlData.substr(0, (urlData.length - 1));
         return urlData;
       }
--- tests.js
+++ tests.js
@@ -12,6 +12,10 @@ const urlParams = new URLSearchParams(window.location.search);
 const PID = urlParams.get('pid'); // 'your spurwing provider-id';
 const KEY = urlParams.get('key')  // 'your spurwing api-key'
 
+if (!PID || !KEY) {
+  console.error('missing pid / key')
+}
+
 async function runner(testname, func) {
   try {
     console.log(testname, 'STARTED')
@@ -27,34 +31,35 @@ async function runner(testname, func) {
 
   await runner('TEST 1', (async() => {
     let sp = new Spurwing();
-    let tz = "Europe/Brussels";
 
-    let A = await sp.get_appointment_types(PID, true)
+    let A = await sp.get_appointment_types(PID)
     console.log({A})
     A.should.have.lengthOf(3) // default 3
 
-    let B = await sp.get_days_available(PID, A[0].id, dateNow(), tz, false)
+    let appointment_type_id = A[0].id;
+
+    let B = await sp.get_days_available(PID, appointment_type_id)
     console.log({B})
     expect(B.days_available.length).to.be.at.least(1) // at least one day available this month
 
-    let C = await sp.get_slots_available(PID, A[0].id, dateNow(), dateTomorrow(), false)
+    let C = await sp.get_slots_available(PID, appointment_type_id, dateNow(), dateTomorrow())
     console.log({C})
     expect(C.slots_available.length).to.be.at.least(10) // each day has 96 15min slots (60*24 / 15 == 96)
 
     let slot = C.slots_available[5].date
-    let D = await sp.complete_booking(PID, A[0].id, slot, tz, 'Ilya', 'Nevolin', 'ilya2@nevolin.be', '111-111-7777', 'My Contact Type');
+    let D = await sp.complete_booking(PID, appointment_type_id, 'ilya2@nevolin.be', 'Ilya', 'Nevo', slot, 'Test booking');
     console.log({D})
     D.should.have.property('appointment')
     expect(D.appointment.length).to.equal(60)
 
-    let E = await sp.list_appointments(KEY, 1000, 0, PID)
+    let E = await sp.list_appointments(KEY, 1000, 0)
     console.log({E})
     E.should.have.property('data')
     E.data.should.have.property('appointments')
     expect(E.data.appointmentsCount).to.be.at.least(1)
 
     let apid = D.appointment.id;
-    let F = await sp.delete_appointment(apid, KEY)
+    let F = await sp.delete_appointment(KEY, apid)
     console.log({F})
     F.should.have.property('data')
     F.data.should.have.property('appointment')
@@ -64,7 +69,7 @@ async function runner(testname, func) {
 
   await runner('TEST 2', (async() => {
     let sp = new Spurwing();
-    let A = await sp.list_appointments(KEY, 1000, 0, PID)
+    let A = await sp.list_appointments(KEY, 1000, 0)
     console.log({A})
     A.should.have.property('data')
     A.data.should.have.property('appointments')
